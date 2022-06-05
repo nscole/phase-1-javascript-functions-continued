4d6bac641f853a8474eba08db1002cb4303beed7
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 18:20:43 +1000

    Completed assignemnt

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ const mondayWork = function (activity = 'go to the office') {
     return `This Monday, I will ${activity}.`
   }
 
+  mondayWork ()
+
+
+// Test 7-9
+
+
+function wrapAdjective (visualFlair="*"){
+    return function(parameter="special"){
+      return `You are ${visualFlair+parameter+visualFlair}!`
+    }
+  }
+/*
+
+*/
