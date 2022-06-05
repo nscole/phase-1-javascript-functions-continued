682739ce786ee5f2948a5fc302595397c35b001f
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 17:52:12 +1000

    pass 3 tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+
+/*
+describe("index.js", () => {
+  describe("defines saturdayFun function declaration as specified", function() {
+    it("function exists", function() {
+      expect(saturdayFun).to.exist
+    })
+
+    it("uses the default activity 'roller-skate' when no arguments are passed", function() {
+      expect(saturdayFun()).to.equal("This Saturday, I want to roller-skate!")
+    })
+
+    it("permits the default activity to be overriden", function() {
+      expect(saturdayFun("bathe my dog")).to.equal("This Saturday, I want to bathe my dog!")
+    })
+  })
+*/
